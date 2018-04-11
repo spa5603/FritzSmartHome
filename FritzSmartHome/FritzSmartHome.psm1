6baa713936cdce3067c914e17518a39430de9545
spa5603 <37816461+spa5603@users.noreply.github.com>
2018-04-11 21:52:57 +0200

    Add files via upload

--- FritzSmartHome/FritzSmartHome.psm1
+++ FritzSmartHome/FritzSmartHome.psm1
@@ -20,7 +20,7 @@ function Get-StringMD5Hash {
     Return $MD5Hash
 }
 
-function Get-FritzBoxSID {
+function Get-FSHSID {
 
     Param (
         [Parameter (Mandatory = $true)][string]$Password,
@@ -41,7 +41,7 @@ function Get-FritzBoxSID {
     Return $SID
 }
 
-function Get-HtrMeasuredTemp {
+function Get-FSHMeasuredTemp {
 
     Param (
         [Parameter (Mandatory = $true)][string]$Ain,
@@ -54,39 +54,111 @@ function Get-HtrMeasuredTemp {
     Return $Temp
 }
 
-function Get-HtrDevices {
+function Get-FSHDevices {
     
     Param (
         [Parameter (Mandatory = $true)][string]$SID
         )
 
-        [xml]$RawData = (Invoke-WebRequest -Uri "http://fritz.box/webservices/homeautoswitch.lua?&switchcmd=getdevicelistinfos&sid=$SID").Content
+       [xml]$RawData = (Invoke-WebRequest -Uri "http://fritz.box/webservices/homeautoswitch.lua?&switchcmd=getdevicelistinfos&sid=$SID").Content
 
         $Devices = $RawData.devicelist.device
 
-        $CustomObjects = $null
+        [array]$PowerOutlet = $null
+        [array]$RadiatorControl = $null
+        [array]$CustomRadiatorControllerObjects = $null
+        [array]$CustomPowerOutletObjects = $null
 
+        # Separation into RadiatorControl and PowerOutlet
         foreach ($Device in $Devices) {
+            if ($Device.InnerXML | Select-String -Pattern 'powermeter') 
+            {
+                [array]$PowerOutlets += $Device
+            }
+            elseif ($Device.InnerXML | Select-String -Pattern 'hkr')
+            {
+                [array]$RadiatorController += $Device
+            }
+            else
+            {
+                [array]$Misc += $Device
+            }
+        }
 
-        $CustomObject = [PSCustomObject]@{
-            AIN =            [string]$Device.identifier -replace ' ',''
-            Name =           [string]$Device.name
-            Manufacturer =   [string]$Device.manufacturer
-            ProductName =    [string]$Device.productname
-            FirmWare =       [string]$Device.fwversion
-            Active =         ([bool]$DeviceLock = ([int]$Device.present))
-            Temperature =    [double]($Device.hkr.tist)/2
-            Offset =         [double]$Device.temperature.offset
-            TempDesired =    [double]($Device.hkr.tsoll)/2
-            TempEco =        [double]($Device.hkr.absenk)/2
-            TempComfort =    [double]($Device.hkr.komfort)/2
-            DeviceLock =     ([bool]$DeviceLock = ([int]$Device.hkr.devicelock))
-            ReplaceBattery = ([bool]$Replacebattery = ([int]$Device.hkr.batterylow))
+        foreach ($RadiatorControl in $RadiatorController) {
+
+        if (($RadiatorControl.temperature.offset) -ne 0) {[double]$offset = ($RadiatorControl.temperature.offset).Insert(2,'.')}
+        else {[double]$offset = ($RadiatorControl.temperature.offset)}
+
+        $CustomRadiatorControllerObject = [PSCustomObject]@{
+            'DeviceId(AIN)' =           [string]$RadiatorControl.identifier -replace ' ',''
+            'DeviceName' =              [string]$RadiatorControl.name
+            'Manufacturer' =            [string]$RadiatorControl.manufacturer
+            'ProductName' =             [string]$RadiatorControl.productname
+            'DeviceType' =              [string]'Radiator Controller'
+            'FirmWare' =                [string]$RadiatorControl.fwversion
+            'DECTConnection' =          ([bool]$DeviceLock = ([int]$RadiatorControl.present))
+            'Temperature(C)' =          [double]($RadiatorControl.hkr.tist)/2
+            'TemperatureOffset(C)' =    $offset
+            'TempDesired(C)' =          [double]($RadiatorControl.hkr.tsoll)/2
+            'TempEco(C)' =              [double]($RadiatorControl.hkr.absenk)/2
+            'TempComfort(C)' =          [double]($RadiatorControl.hkr.komfort)/2
+            'DeviceLock' =              ([bool]$DeviceLock = ([int]$RadiatorControl.hkr.devicelock))
+            'ReplaceBattery' =          ([bool]$Replacebattery = ([int]$RadiatorControl.hkr.batterylow))
             }
 
-        [array]$CustomObjects += $CustomObject
+        $CustomRadiatorControllerObject
 
         }
 
-    Return $CustomObjects
+        foreach ($PowerOutlet in $PowerOutlets) {
+
+        if (($PowerOutlet.temperature.offset) -ne 0) {[doubel]$offset = ($PowerOutlet.temperature.offset).Insert(2,'.')}
+        else {[double]$offset = ($PowerOutlet.temperature.offset)}
+
+        $CustomPowerOutletObject = [PSCustomObject]@{
+            'DeviceId(AIN)' =           [string]$PowerOutlet.identifier -replace ' ',''
+            'DeviceName' =              [string]$PowerOutlet.name
+            'Manufacturer' =            [string]$PowerOutlet.manufacturer
+            'ProductName' =             [string]$PowerOutlet.productname
+            'DeviceType' =              [string]'Power Outlet'
+            'FirmWare' =                [string]$PowerOutlet.fwversion
+            'DECTConnection' =          ([bool]$Active = ([int]$PowerOutlet.present))
+            'DeviceMode' =              [string]$PowerOutlet.switch.mode
+            'WorkingState' =            ([bool]$Working = ([int]$PowerOutlet.switch.state))
+            'CapacityCurent(W)' =       [double]($PowerOutlet.powermeter.power)/1000
+            'Temperature(C)' =          [double]($PowerOutlet.temperature.celsius).Insert(2,'.')
+            'TemperatureOffset(C)' =    $offset
+            'DeviceLock' =              ([bool]$DeviceLock = ([int]$PowerOutlet.switch.devicelock))
+            }
+
+        $CustomPowerOutletObject
+
+        }
+
+}
+
+function Set-FSHTemperature {
+    
+    Param (
+        [Parameter (Mandatory = $true)][string]$Ain,
+        [Parameter (Mandatory = $true)][string]$SID,
+        [Parameter (Mandatory = $true)][string]$Temperature
+        )
+
+    # Check the Value and recalculate
+    if ($Temperature -match "\d") {
+        [double]$Temperature = $Temperature
+        }
+    
+    switch ($Temperature)
+    {
+        ('On') {$Temp = '254'}
+        ('Off'){$Temp = '253'}
+        {$_ -ge 8 -and $_ -le 28} {[string]$Temp = ($Temperature*2)}
+        default {Write-Error 'Temperature is out of range. range is 8-28 C.' -Category InvalidArgument -ErrorAction Stop}
+    }
+
+(Invoke-WebRequest -Uri ("http://fritz.box/webservices/homeautoswitch.lua?ain=$Ain&switchcmd=sethkrtsoll&sid=$SID") -Body @{param = $Temp}).StatusDescription
+
 }
